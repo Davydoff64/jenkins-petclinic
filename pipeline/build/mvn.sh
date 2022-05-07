0d2d9bd554044b590cdefb3ededf2680a4882eef
Davydoff64 <96378267+Davydoff64@users.noreply.github.com>
2022-05-08 00:38:10 +0400

    change mvn.sh 1

--- pipeline/build/mvn.sh
+++ pipeline/build/mvn.sh
@@ -6,5 +6,5 @@ echo "****************"
 
 WORKSPACE=/home/arkady/jenkins/jenkins_home/workspace/pipe-project
 
-docker run --rm -v ~/jenkins/pipeline/Test-mysql-petclinic:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5.4-alpine "$@"
+docker run --rm -v ~/jenkins/pipeline/Test-mysql-petclinic:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.5.4-alpine mvn clean package
 
